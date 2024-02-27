f1d56d802fd8b19b84cc4cce0d45cb1e8eb82dcf
Nando Delgado <nando.emmanuel@gmail.com>
2024-02-27 11:27:19 -0300

    Updated nextConfig basePath and to export static files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	basePath: "/elproximoferiado",
+	output: "export",
+  	reactStrictMode: true,
+};
 
 export default nextConfig;
